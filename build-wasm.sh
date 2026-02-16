df7461b8c9f963e067770ec88e3b81f53921d48c
buyoh <15198247+buyoh@users.noreply.github.com>
2026-02-16 21:58:22 +0900

    Move whitespace-interpreter-tests to done-task/

---
+++ build-wasm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+cd "$(dirname "$0")"
+
+# release 
+cargo build --release \
+  --target wasm32-unknown-unknown --lib \
+  --no-default-features --features wasm
+wasm-pack build --target bundler --features wasm
+
+# debug
+cargo build \
+  --target wasm32-unknown-unknown --lib \
+  --no-default-features --features wasm
+wasm-pack build --target bundler --features wasm --dev --out-dir pkg-dev
