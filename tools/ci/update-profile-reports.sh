d493cc9b7495111cf742b620de01537cc7f726ba
buyoh <15198247+buyoh@users.noreply.github.com>
2026-02-26 02:29:14 +0900

    add alloc profile

--- tools/ci/update-profile-reports.sh
+++ tools/ci/update-profile-reports.sh
@@ -5,15 +5,14 @@ cd "$(dirname "$0")/../../"
 VERSION_NUMVER=0
 COMMIT_NUMBER=$(git rev-list --count HEAD)
 
-PROFILE_FILENAME="profile-c${COMMIT_NUMBER}v${VERSION_NUMVER}.json"
+PROFILE_FILENAME_PREFIX="profile-c${COMMIT_NUMBER}v${VERSION_NUMVER}"
 
-if [ -f "./profile-reports/${PROFILE_FILENAME}" ]; then
-  echo "Profile report already exists: ${PROFILE_FILENAME}"
-  exit 0
-fi
+cargo run --example ws_profiler -- --json \
+  > "./profile-reports/${PROFILE_FILENAME_PREFIX}.json"
+cargo run --example ws_profiler -- --json --std-ext alloc \
+  > "./profile-reports/${PROFILE_FILENAME_PREFIX}-alloc.json"
 
-cargo run --example ws_profiler -- --json > "./profile-reports/${PROFILE_FILENAME}"
 if [ -f "./profile-reports/profile-latest.json" ]; then
   rm "./profile-reports/profile-latest.json"
 fi
-ln -s "${PROFILE_FILENAME}" "./profile-reports/profile-latest.json"
+ln -s "${PROFILE_FILENAME_PREFIX}.json" "./profile-reports/profile-latest.json"
